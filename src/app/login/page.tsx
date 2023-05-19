706e364572e592dcff03a290b71235d355078266
siddharth <siddharth@godspeed.systems>
2023-05-20 00:26:28 +0530

    login page ready

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 function Login() {
   return (
-    <div>Login</div>
-  )
+    <section className="bg-gray-50">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <Link
+            href="https://www.godspeed.systems"
+            target="_blank"
+            className="flex flex-col items-center mb-6 text-[10px] font-semibold text-gray-900"
+          >
+            <Image
+              width={200}
+              height={40}
+              className="mr-2"
+              src="/Logo.png"
+              alt="logo"
+            />
+            GPT Powered Documentation for Godspeed Framework
+          </Link>
+
+        <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 ">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+              Sign in to your account
+            </h1>
+            <form className="space-y-4 md:space-y-6" action="#">
+              <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900">
+                  Your email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  placeholder="name@company.com"
+                  required={true}
+                />
+              </div>
+              <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  required={true}
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
+              >
+                Sign in
+              </button>
+              <p className="text-sm font-light text-gray-500">
+                Don’t have an account yet?{" "}
+                <Link
+                  href="/contact"
+                  className="font-medium text-primary-600 hover:underline"
+                >
+                  Contat Admin
+                </Link>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Login
+export default Login;
